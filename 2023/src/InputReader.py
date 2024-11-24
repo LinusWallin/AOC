c81acdc9fa7a86bd00950017a504371b643e9a92
Linus Wallin <linuswallin@live.se>
2024-11-24 17:46:25 +0100

    init: init for AOC 2023

---
+++ 2023/src/InputReader.py
@@ -0,0 +1,26 @@
+class InputReader:
+    """A class to read AOC problem input
+    """
+    def __init__(self, day, DEBUG):
+        """Initializes arguments
+
+        Args:
+            day (str): AOC day number
+            DEBUG (bool): Option for debugging
+        """
+        self.day = day
+        self.DEBUG = DEBUG
+
+    def ReadInput(self, splitSymbol):
+        """Reads AOC input file and splits it at a defined symbol. The split list is then
+        returned
+
+        Args:
+            splitSymbol(str): Symbol to split the data of the text file by
+
+        Returns:
+            list: input list for the specific problem
+        """
+        with open(f"./data/{self.day}{"DEBUG" if self.DEBUG else ""}.txt", "r") as inputFile:
+            input:list[str] = inputFile.read().split(splitSymbol)
+        return input
