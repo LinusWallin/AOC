cb9f42d2c200f08c0c27af0640b85ed64e44d473
Linus Wallin <linuswallin@live.se>
2024-12-01 19:37:56 +0100

    feat: day 2 part 2

--- 2024/src/Day1.py
+++ 2024/src/Day1.py
@@ -2,10 +2,10 @@ from InputReader import InputReader
 import numpy as np
 
 def splitData(data):
-    return np.array([row.split("   ") for row in data])
+    return (np.array([row.split("   ") for row in data])).astype(int)
 
 def sortCols(data):
-    data = data.astype(int)
+    #data = data.astype(int)
     data = np.sort(data, axis=0)
     return data
 
@@ -13,11 +13,26 @@ def diffSum(matrix):
     diff = abs(np.subtract(matrix[:,0], matrix[:,1]))
     return sum(diff)
 
+def occurances(data, num):
+    occ = 0
+    for i in range(len(data)):
+        if data[i,1] == num:
+            occ += 1
+        elif data[i,1] > num:
+            break
+    return occ
+
+def calcOccSum(data):
+    s = 0
+    for num in data[:,0]:
+        s += num * occurances(data, num)
+    return s
+
 if __name__ == "__main__":
     inputReader = InputReader(1, False)
     data = inputReader.ReadInput("\n")
     dataMat = splitData(data)
     sortedData = sortCols(dataMat)
     print(diffSum(sortedData))
+    print(calcOccSum(sortedData))
     
-
